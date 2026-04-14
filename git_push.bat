abc4a596789940c7e56d5b7fdf49800eb22bb936
NagaAbhishek03 <96488726+NagaAbhishek03@users.noreply.github.com>
2026-04-14 17:20:18 +0530

    added model files

--- git_push.bat
+++ git_push.bat
@@ -1,12 +1,9 @@
 @echo off
-echo Running Git commands to upload your project to GitHub...
+echo Running Git commands to upload your newly added files to GitHub...
 
-git init
 git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/NagaAbhishek03/hackton.git
-git push -u origin main
+git commit -m "added model files"
+git push
 
 echo.
 echo Process complete! Please check if there are any errors above.
